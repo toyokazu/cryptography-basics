66acabfe4558db16b170ecb2ab0ea4e8ad468f14
Toyokazu Akiyama <toyokazu@gmail.com>
2013-08-03 11:28:55 +0900

    show key and binary plain characters at one_time_pad exercise answer page

--- app/controllers/one_time_pad_exercises_controller.rb
+++ app/controllers/one_time_pad_exercises_controller.rb
@@ -12,17 +12,21 @@ class OneTimePadExercisesController < ApplicationController
     check(session[:answer][:encrypted] == @user_answer)
     @user_answer = split_binary_str(@user_answer)
     @answer = split_binary_str(session[:answer][:encrypted])
+    @plain = split_binary_str(session[:answer][:plain])
+    @key_array = split_binary_str(session[:answer][:key])
   end
   
   def exercise2
     generate_answer1
-    @key_array = split_binary_str(@key)
     @encrypted = split_binary_str(session[:answer][:encrypted])
+    @key_array = split_binary_str(@key)
   end
   
   def check2
     @user_answer = params["answer"]
     check(session[:answer][:word] == @user_answer)
     @encrypted = split_binary_str(session[:answer][:encrypted])
+    @plain = split_binary_str(session[:answer][:plain])
+    @key_array = split_binary_str(session[:answer][:key])
   end
 end
